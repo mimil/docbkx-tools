de4938e349a71f8e1ccc85f082890b276610c34d
mimilowns <mimilowns@87431226-bd31-0410-8fac-cb584273b434>
2014-01-19 18:52:45 +0000

    #110, update of copyright headers

--- trunk/docbkx-builder-maven-plugin/src/main/java/com/agilejava/maven/docbkx/GeneratorMojo.java
+++ trunk/docbkx-builder-maven-plugin/src/main/java/com/agilejava/maven/docbkx/GeneratorMojo.java
@@ -1,18 +1,24 @@
 /*
- * Copyright 2006 Wilfred Springer
+ * #%L
+ * Docbkx Maven Plugin Builder
+ * %%
+ * Copyright (C) 2006 - 2014 Wilfred Springer, Cedric Pronzato
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 package com.agilejava.maven.docbkx;
 
-import com.agilejava.maven.docbkx.ZipFileProcessor.ZipEntryVisitor;
 import com.agilejava.maven.docbkx.spec.Parameter;
 import com.agilejava.maven.docbkx.spec.Specification;
 
@@ -30,8 +36,6 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
-
-import org.codehaus.plexus.util.SelectorUtils;
 
 import org.jaxen.JaxenException;
 
