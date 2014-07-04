544632ec6b8236f67a23c56a01e72b8bd8910c3b
mimilowns <mimilowns@87431226-bd31-0410-8fac-cb584273b434>
2014-07-04 08:11:59 +0000

    #115, mimetype is not anymore compressed for epub3 output, pathc contributed by ctron1202

--- docbkx-maven-plugin/src/main/java/com/agilejava/docbkx/maven/AbstractEpub3Mojo.java
+++ docbkx-maven-plugin/src/main/java/com/agilejava/docbkx/maven/AbstractEpub3Mojo.java
@@ -19,27 +19,20 @@
  */
 package com.agilejava.docbkx.maven;
 
-/*
- * Copyright 2013 Cedric Pronzato
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.zip.ZipArchiver;
+import org.codehaus.plexus.archiver.zip.ZipEntry;
+import org.codehaus.plexus.archiver.zip.ZipOutputStream;
 import org.codehaus.plexus.util.FileUtils;
 
 import javax.xml.transform.Transformer;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.util.zip.CRC32;
 
 /**
  * A dedicated base class for plugins generating ePub version 3 output, in order to allow
@@ -68,6 +61,10 @@ public abstract class AbstractEpub3Mojo extends AbstractMojoBase {
 
   }
 
+  private static byte [] makeMimetype () throws UnsupportedEncodingException {
+      return "application/epub+zip".getBytes ("UTF-8");
+  }
+  
   public void postProcessResult(File result) throws MojoExecutionException {
     super.postProcessResult(result);
 
@@ -75,18 +72,46 @@ public abstract class AbstractEpub3Mojo extends AbstractMojoBase {
 
     // override current container.xml
     try {
-      final URL containerURL = getClass().getResource("/epub/container.xml"); // reuse of container.cml from epub output
+      final URL containerURL = getClass().getResource("/epub/container.epub3.xml"); // reuse of container.cml from epub output
       FileUtils.copyURLToFile(containerURL, new File(targetDirectory, "META-INF" + File.separator + "container.xml"));
     } catch (IOException e) {
       throw new MojoExecutionException("Unable to copy hardcoded container.xml file", e);
     }
 
-
+    final byte [] mimetypeData;
     try {
-      ZipArchiver zipArchiver = new ZipArchiver();
+        mimetypeData = makeMimetype ();
+    } catch ( UnsupportedEncodingException e ) {
+        throw new MojoExecutionException("Unable to create mimetype data", e);
+    }
+    
+    try {
+      // first delete "mimetype" that is already there
+      final File mimetype = new File(targetDirectory,"mimetype");
+      mimetype.delete ();
+        
+      ZipArchiver zipArchiver = new ZipArchiver() {
+          protected void initZipOutputStream ( ZipOutputStream zOut ) throws IOException, ArchiverException
+        {
+            CRC32 crc = new CRC32 ();
+            crc.update(mimetypeData);
+            
+            ZipEntry ze = new ZipEntry ("mimetype");
+            ze.setMethod(ZipEntry.STORED);
+            ze.setSize(mimetypeData.length );
+            ze.setCrc(crc.getValue ());
+            zOut.putNextEntry(ze);
+            zOut.write(mimetypeData);
+        }
+      };
+      
+      // add content
       zipArchiver.addDirectory(targetDirectory);
-      zipArchiver.setCompress(true); // seems to not be a problem to have mimetype compressed
+      zipArchiver.setCompress(true);
+      
+      // set output file
       zipArchiver.setDestFile(new File(targetDirectory.getParentFile(), result.getName())); // copy it to parent dir
+      
       zipArchiver.createArchive();
 
       getLog().debug("epub file created at: " + zipArchiver.getDestFile().getAbsolutePath());
